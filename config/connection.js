bf079e62bb54af0f2f169fcea70282c49d18075d
david morse <davidbryanmarshall@gmail.com>
2017-10-30 01:02:05 -0700

    testing heroku mysql with 'ClearDB MySQL'

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,19 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var local_connection = mysql.createConnection({
   port: 3306,
   host: 'localhost',
   user: 'root',
   password: 'root',
   database: 'burger_db'
+});
+
+//  mysql://b25f5f51735c83:3aa74aac@us-cdbr-iron-east-05.cleardb.net/heroku_44781f3abe4cedc?reconnect=true
+var connection = mysql.createConnection({
+  host     : 'us-cdbr-iron-east-05.cleardb.net',
+  database : 'heroku_28437b49d76cc53',
+  user     : 'b25f5f51735c83',
+  password : '3aa74aac'
 });
 
 connection.connect(function(err) {
