523c972d8512fd31b1ddefb995cf04b0ecae16b7
david morse <davidbryanmarshall@gmail.com>
2017-10-30 02:30:10 -0700

    switching to heroku database connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql');
 
-var local_connection = mysql.createConnection({
-  port: 3306,
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'burger_db'
-});
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'root',
+//   database: 'burger_db'
+// });
 
 //  mysql://b25f5f51735c83:3aa74aac@us-cdbr-iron-east-05.cleardb.net/heroku_44781f3abe4cedc?reconnect=true
 var connection = mysql.createConnection({
@@ -15,6 +15,14 @@ var connection = mysql.createConnection({
   user     : 'b25f5f51735c83',
   password : '3aa74aac'
 });
+
+// 01:57:24 USE heroku_28437b49d76cc53  Error Code: 1044. Access denied for user 'b25f5f51735c83'@'%' to database 'heroku_28437b49d76cc53'  0.099 sec
+
+// CREATE USER 'b25f5f51735c83'@'%' IDENTIFIED BY "3aa74aac";
+// GRANT ALL PRIVILEDGES ON heroku_28437b49d76cc53.* TO 'b25f5f51735c83'@'%';
+
+// grant all on heroku_28437b49d76cc53.* to b25f5f51735c83@%;
+// GRANT ALL ON heroku_28437b49d76cc53.* TO 'b25f5f51735c83@%';
 
 connection.connect(function(err) {
   if (err) {
