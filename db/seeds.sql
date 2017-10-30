b53749bafa852f1418689a71063792e2615f8186
david morse <davidbryanmarshall@gmail.com>
2017-10-29 22:35:20 -0700

    working MySQL schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('Boca');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Portobello', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fakin\' Bacon', true);
+
+SELECT * FROM burgers;
